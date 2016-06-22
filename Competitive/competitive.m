19caad31bc76d19d2936a412f31d98612b3a585a
CatStark <zuxi.w13@gmail.com>
2016-06-22 13:27:03 +0200

    Update competitive.m

--- Competitive/competitive.m
+++ Competitive/competitive.m
@@ -1,3 +1,7 @@
+% Susana G
+% CUCEI Universidad de Guadalajara 2012
+% Redes Neuronales Artificiales
+
 clear all;
 close all;
 P = [.78 .8  .79 .83 .85 .4 .45 .42 .43 .39 .42 .9  .94 .91 .95 .98 .6 .63 .59 .61 .58;
@@ -7,9 +11,9 @@ plot(P(1,:),P(2,:),'+r');
 
 hold on
 grid on
-%net=newc([0 1;0 1],4,.2);
-%net=newc([0 1;0 1],3,.2);
-net=newc([0 1;0 1],5,.2);
+%net=newc([0 1;0 1],4,.2); %4 neurons
+%net=newc([0 1;0 1],3,.2); %3 neurons
+net=newc([0 1;0 1],5,.2); %neurons
 w=net.IW{1};
 plot(w(:,1),w(:,2),'ob');
 
